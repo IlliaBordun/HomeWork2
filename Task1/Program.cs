4a66d0155ccf590fe0e30dd61a84d526a6eb580b
Illia <Illia@WIN-2GILFBI2O36>
2023-11-13 21:03:03 +0200

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿class Checker
+{
+    static void Main()
+    {
+        double first = Convert.ToDouble(Console.ReadLine());
+        double second = Convert.ToDouble(Console.ReadLine());
+        double third = Convert.ToDouble(Console.ReadLine());
+
+        int start = -5;
+        int end = 5;
+
+        if (first >= start && first <= end &&
+            second >= start && second <= end &&
+            third >= start && third <= end)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+}
