59dddea1b2ddb57d54bf535f902c42346a472948
Illia <Illia@WIN-2GILFBI2O36>
2023-11-14 20:17:55 +0200

    add Tasks

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿class Checker
+{
+    public static void Main()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int[] numbers = { a, b, c };
+        Console.WriteLine(numbers.Max());
+        Console.WriteLine(numbers.Min());
+    }
+}
